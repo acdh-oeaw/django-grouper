9dea5351488f691bedaacaea74f371053c0fe8f8
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-06-21 13:00:32 +0200

    feat: allow to filter queryset before doing the grouping

--- django_grouper/views.py
+++ django_grouper/views.py
@@ -1,13 +1,16 @@
+from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
 from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect
+
 
 from .utils import group_queryset
 
 
 class BaseView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
-        app_label, model = request.GET.get("content_type", ".").split(".")
+        app_label, model = request.GET.get("group_content_type", ".").split(".")
         self.django_content_type = get_object_or_404(ContentType, app_label=app_label, model=model)
         return super().dispatch(request, *args, **kwargs)
 
@@ -16,7 +19,7 @@ class Grouper(BaseView):
     template_name = "django_grouper/grouper.html"
 
     def get_context_data(self, *args, **kwargs):
-        fields = self.request.GET.getlist("fields", [])
+        fields = self.request.GET.getlist("group_fields", [])
 
         ctx = super().get_context_data(*args, **kwargs)
         ctx["content_type"] = self.django_content_type
@@ -24,7 +27,10 @@ class Grouper(BaseView):
         return ctx
 
     def get_queryset(self):
-        return self.django_content_type.model_class().objects.all()
+        qs = self.django_content_type.model_class().objects.all()
+        if hasattr(settings, "GROUP_FILTER"):
+            qs = settings.GROUP_FILTER(qs, self.request)
+        return qs
 
 
 class Group(BaseView):
@@ -42,6 +48,10 @@ class Group(BaseView):
     def post(self, request, *args, **kwargs):
         ctx = self.get_context_data()
         ctx["merged_ids"] = []
+        newinstance = self.django_content_type.model_class().objects.create()
         for merge_id in request.POST.getlist("to_merge"):
+            mergeobject = get_object_or_404(self.django_content_type.model_class(), pk=merge_id)
+            mergeobject.grouped_into = newinstance
+            mergeobject.save()
             ctx["merged_ids"].append(merge_id)
-        return self.render_to_response(ctx)
+        return HttpResponseRedirect(newinstance.get_absolute_url())
